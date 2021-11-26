72f36433b734666ca8c74c6694de6818df6b1fd7
Levi Wilkerson <chelleas123@gmail.com>
2021-11-25 21:31:45 -0500

    added silverwind logo to page and footer styles

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,16 +5,42 @@ import { ReactComponent as TikTokLogo } from "../Images/tiktok.svg";
 export default function Footer() {
   return (
     <footer className="footer">
-      <div className="footer-container">
-        <div className="footer-start"></div>
+      <div className="footer-container container">
+        <div className="footer-start">
+          <ul>
+            <li className="footer__title">Help</li>
+            <li className="clickable footer__li">Contact Us</li>
+            <li className="clickable footer__li">Delivery Information</li>
+            <li className="clickable footer__li">Return Policy</li>
+            <li className="clickable footer__li">FAQ</li>
+          </ul>
+          <ul>
+            <li className=" footer__title">My Account</li>
+            <li className="clickable footer__li">Login</li>
+            <li className="clickable footer__li">Register</li>
+          </ul>
+          <ul>
+            <li className=" footer__title">Pages</li>
+            <li className="clickable footer__li">FAQ</li>
+            <li className="clickable footer__li">All Products</li>
+            <li className="clickable footer__li">About Me</li>
+            <li className="clickable footer__li">Portfolio</li>
+          </ul>
+        </div>
         <div className="footer-end">
-          <div className="footer-SocialsTop">
-            <h6>More from SilverWind</h6>
+          <div className="footer-socialsTop">
+            <h6 className="footer__title">More from SilverWind</h6>
           </div>
-          <div className="footer-SocialsBottom">
-            <InstagramLogo />
-            <TikTokLogo />
+          <div className="footer-socialsBottom">
+            <InstagramLogo className="clickable" />
+            <TikTokLogo className="clickable" />
           </div>
+        </div>
+      </div>
+      <div className="footer-legal">
+        <div className="footer-container container">
+          <div className="footerLegal-start">Legal Start</div>
+          <div className="footerLegal-end">Legal End</div>
         </div>
       </div>
     </footer>
